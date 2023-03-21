a55f4fe4b78cd5a1b266185a3072d933166d1c0d
xvtn <me@xvtn.ru>
2023-03-22 00:17:00 +0500

    summary, bot link, new errors

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,90 @@
 <script setup>
 import { onMounted, reactive } from 'vue';
-import { getCard } from './tts';
+import { vgetCard } from './tts';
 
 const state = reactive({
-  is_loading: false,
-  is_error: false,
+  status: 'ok',
   currentTimeout: 0,
-  card_number: localStorage.getItem('card_number') || null,
+  cardNumber: localStorage.getItem('card_number') || null,
+
   card: {},
   error: {},
 });
 
 const fetchCard = async () => {
-  if (!state.card_number) return;
+  if (!state.cardNumber) return;
+
   state.card = {};
-  state.is_loading = true;
-  state.is_error = false;
   state.error = {};
+  state.status = 'loading';
 
   try {
-    const { data } = await getCard(state.card_number);
-    if (data.error) {
-      state.is_error = true;
-      state.error = data;
-    }
-
-    state.card = data;
-    state.is_loading = false;
-    localStorage.setItem('card_number', data.id);
-  } catch (err) {
-    state.is_loading = false;
-    state.is_error = true;
-    state.error = err?.response?.data || { message: 'Какая-то внутренняя ошибка ¯\\_(ツ)_/¯', details: err };
+    const card = await vgetCard(state.cardNumber);
+    state.card = card;
+    state.status = 'ok';
+    localStorage.setItem('card_number', card.id);
+  } catch (error) {
+    state.error = {
+      title: error.title,
+      message: error.message,
+      details: error.details,
+    };
+    state.status = 'error';
   }
 };
 
-const cardNumberRegex = /(\d{19})/;
-const validateCardNumber = () => {
-  if (cardNumberRegex.test(state.card_number)) {
-    clearTimeout(state.currentTimeout);
-    state.currentTimeout = setTimeout(async () => {
-      fetchCard();
-    }, 1000);
-  }
+const onInput = () => {
+  if (!/(\d{19})/.test(state.cardNumber)) return;
+
+  clearTimeout(state.currentTimeout);
+  state.currentTimeout = setTimeout(async () => { fetchCard(); }, 1000);
 };
 
-onMounted(() => {
-  fetchCard();
-});
+onMounted(() => { fetchCard(); });
 
 window.state = state;
-
 </script>
 
 <template>
   <div class="view h-full w-full max-w-md flex flex-col gap-4 items-center p-4 overflow-scroll">
-    <div class="header w-full flex flex-col">
-      <h1 class="title text-lg text-white font-bold">Мой ТТС <span class="ml-1 text-sm text-neutral-500 font-normal">beta</span></h1>
-      <h2 class="subtitle text-neutral-500">Просмотр баланса транспортной карты Тюменской области (ТТС)</h2>
+    <div class="header flex flex-col">
+      <div class="title">Мой ТТС</div>
+      <div class="subtitle">Просмотр баланса транспортной карты Тюменской области (ТТС)</div>
     </div>
 
     <div class="card-number w-full flex flex-col rounded-md">
-      <label class="mb-1 text-sm text-neutral-500">Номер карты</label>
-      <input @input="validateCardNumber" v-model="state.card_number" type="text" class="!outline-none w-full px-4 py-2 rounded-lg bg-neutral-800 text-white placeholder:text-neutral-500 disabled:text-neutral-500" placeholder="9990057230080149643">
+      <label>Номер карты</label>
+      <input
+        @input="onInput"
+        v-model="state.cardNumber"
+        type="text"
+        placeholder="9990057230080149643"
+      >
     </div>
 
-    <div v-if="state.is_loading" class="spinner w-full max-w-xl flex flex-col items-center">
+    <div v-if="state.status === 'loading'" class="spinner w-full max-w-xl flex flex-col items-center">
       <svg class="text-neutral-600 animate-spin" fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
         <path clip-rule="evenodd" d="M16.394 5.077A8.2 8.2 0 0 0 4.58 15.49a.9.9 0 0 1-1.628.767A10 10 0 1 1 12 22a.9.9 0 0 1 0-1.8 8.2 8.2 0 0 0 4.394-15.123z" fill="currentColor" fill-rule="evenodd"/>
       </svg>
     </div>
 
-    <div v-if="state.is_error" class="error w-full rounded-md text-white bg-neutral-800 px-4 py-2 flex flex-col">
-      <div class="title text-lg font-bold">Упс! Произошла ошибка :(</div>
-      <div class="message text-neutral-500">{{ state.error.message }}</div>
-      <div class="mt-2 details font-mono text-sm text-neutral-500">{{ state.error.details }}</div>
+    <div v-if="state.status === 'error'" class="error w-full rounded-md text-white bg-neutral-800 p-4 flex flex-col">
+      <div class="title">{{ state.error.title || "Что-то пошло не так :(" }}</div>
+      <div class="subtitle">{{ state.error.message }}</div>
+      <div class="details font-mono text-sm text-neutral-500">{{ state.error.details }}</div>
+      <div class="actions mt-2 flex gap-2">
+        <a @click="fetchCard" class="button bg-neutral-700">
+          <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+          <g id="Page-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+              <g id="replay_24">
+                  <polygon id="Shape" points="0 0 24 0 24 24 0 24"></polygon>
+                  <path d="M12,5 C16.418278,5 20,8.581722 20,13 C20,17.418278 16.418278,21 12,21 C7.581722,21 4,17.418278 4,13 C4,12.4477153 4.44771525,12 5,12 C5.55228475,12 6,12.4477153 6,13 C6,16.3137085 8.6862915,19 12,19 C15.3137085,19 18,16.3137085 18,13 C18,9.6862915 15.3137085,7 12,7 L12,10.0957793 C12,10.1952857 11.9629113,10.2912196 11.895976,10.3648484 C11.7473732,10.5283115 11.4943939,10.5403581 11.3309309,10.3917553 L7.04262272,6.49329338 C7.02697148,6.47906498 7.01200627,6.46409977 6.99777787,6.44844853 C6.75010658,6.17601012 6.77018431,5.7543779 7.04262272,5.50670662 L11.3309309,1.60824468 C11.4045596,1.54130941 11.5004936,1.5042207 11.6,1.5042207 C11.8209139,1.5042207 12,1.68330681 12,1.90422072 L12,5 Z" id="Mask" fill="currentColor"></path>
+                </g>
+            </g>
+          </svg>
+          <div class="caption">Повторить</div>
+        </a>
+      </div>
     </div>
 
     <div v-if="state.card.id" class="card w-full flex flex-col gap-2 rounded-md p-4 bg-neutral-800">
@@ -82,10 +92,10 @@ window.state = state;
         <div class="card-type font-medium text-white">Карта «{{ state.card.type }}»</div>
         <div class="card-balance font-bold text-2xl my-1 text-green-500">{{ state.card.balance }} ₽</div>
         <div class="card-number text-sm text-neutral-500">{{ state.card.id }}</div>
-        <div class="card-updated text-sm text-neutral-500">Обновлено: {{ state.card.date }}</div>
+        <div class="card-updated text-sm text-neutral-500">Обновлено: {{ state.card.updated_at }}</div>
       </div>
 
-      <div class="options flex gap-2">
+      <div class="actions flex gap-2">
         <a @click="fetchCard" class="option refresh text-white font-medium flex gap-1 bg-neutral-700 rounded-md py-2 px-4 w-full justify-center cursor-pointer hover:bg-green-500 transition duration-300">
           <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
           <g id="Page-2" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -106,17 +116,22 @@ window.state = state;
       </div>
     </div>
 
-    <div v-if="state.card.id" class="history flex flex-col gap-1 w-full">
+    <div v-if="state.card.id && state.card.history.length" class="history flex flex-col gap-1 w-full">
+      <div class="summary px-4 flex items-center justify-between">
+        <div class="date text-neutral-500 font-medium">{{ state.card.history.at(-1).date.locale('ru').format('D MMMM') }} — {{ state.card.history.at(0).date.locale('ru').format('D MMMM') }}</div>
+        <div class="sum text-neutral-500 font-bold"> -{{ state.card.history_summary }} ₽</div>
+      </div>
+
       <div v-for="event in state.card.history" :key="event.date" class="event flex flex-col bg-neutral-800 p-4 rounded-md gap-2">
         <div class="event-summary w-full flex justify-between items-center">
-          <div class="left flex items-center w-full">
-            <div class="route-number p-2 text-xl text-green-500 font-bold">{{ event.routeNumber }}</div>
-            <div class="route-name p-2 text-white rounded-lg">{{ event.routeName }}</div>
+          <div class="left flex gap-4 items-center w-full">
+            <div class="route-number text-xl text-green-500 font-bold flex items-center justify-center">{{ event.route_number }}</div>
+            <div class="route-name text-white rounded-lg">{{ event.route_name }}</div>
           </div>
 
-          <div class="right flex flex-col items-center">
-            <div class="sum text-green-500 font-bold text-lg rounded-lg">{{ event.parsedSum }}</div>
-            <div class="date text-sm text-center text-neutral-500">{{ event.parsedDate }}</div>
+          <div class="right flex flex-col items-end">
+            <div class="sum text-green-500 font-bold text-lg rounded-lg">-{{ event.sum }} ₽</div>
+            <div class="date text-sm text-right text-neutral-500">{{ event.date.locale('ru').format('D MMMM в HH:mm') }}</div>
           </div>
         </div>
 
@@ -130,8 +145,17 @@ window.state = state;
       </div>
     </div>
 
-    <div class="footer flex flex-col gap-2 text-center text-neutral-500 text-sm">
-      <div class="disclaimer">Данные о транспортной карте предоставлены сервисом <a href="https://tgt72.ru" target="_blank" class="font-medium text-green-500">tgt72.ru</a> Также вы можете воспользоваться <a href="https://vk.com/tts_app" target="_blank" class="font-medium text-green-500">чат-ботом ВКонтакте</a></div>
+    <div class="footer flex flex-col w-full gap-2 text-center text-neutral-500 text-sm">
+      <div class="actions flex gap-2 w-full">
+        <a href="https://vk.com/tts_app" target="_blank" class="button bg-neutral-800">
+          <svg fill="none" height="16" viewBox="0 0 24 24" width="16" xmlns="http://www.w3.org/2000/svg"><g clip-rule="evenodd" fill="currentColor" fill-rule="evenodd"><path d="m5.14631 3.63427c.76215-.4076 1.51182-.53426 3.08147-.53426h1.87222c.4971 0 .9.40294.9.9 0 .49705-.4029.9-.9.9h-1.87222c-1.48667 0-1.87061.12794-2.23259.32153-.33585.17961-.59404.4378-.77365.77365-.19359.36198-.32153.74592-.32153 2.23259v7.54442c0 1.4867.12794 1.8706.32153 2.2326.17961.3359.4378.5941.77365.7737.36198.1936.74592.3215 2.23259.3215h7.54442c1.4867 0 1.8706-.1279 2.2326-.3215.3359-.1796.5941-.4378.7737-.7737.1936-.362.3215-.7459.3215-2.2326v-1.8722c0-.4971.4029-.9.9-.9s.9.4029.9.9v1.8722c0 1.5697-.1267 2.3193-.5343 3.0815-.3473.6495-.8625 1.1647-1.512 1.512-.7622.4076-1.5118.5343-3.0815.5343h-7.54442c-1.56965 0-2.31931-.1267-3.08147-.5343-.64953-.3473-1.16467-.8625-1.51204-1.512-.4076-.7622-.53426-1.5118-.53426-3.0815v-7.54442c0-1.56965.12666-2.31932.53426-3.08147.34738-.64953.86251-1.16466 1.51204-1.51204z"/><path d="m14 4.00001c0-.49706.403-.9.9-.9h5.1c.4971 0 .9.40294.9.9v5.1c0 .49705-.4029.89999-.9.89999-.497 0-.9-.40294-.9-.89999v-2.92721l-6.5636 6.5636c-.3514.3515-.9213.3515-1.2728 0-.3514-.3515-.3514-.9213 0-1.2728l6.5636-6.56359h-2.9272c-.497 0-.9-.40295-.9-.9z"/></g></svg>
+          <div class="caption">Бот ВКонтакте</div>
+        </a>
+        <a href="https://vk.com/tsivxrev" target="_blank" class="button bg-neutral-800">
+          <svg fill="none" height="16" viewBox="0 0 24 24" width="16" xmlns="http://www.w3.org/2000/svg"><g clip-rule="evenodd" fill="currentColor" fill-rule="evenodd"><path d="m5.14631 3.63427c.76215-.4076 1.51182-.53426 3.08147-.53426h1.87222c.4971 0 .9.40294.9.9 0 .49705-.4029.9-.9.9h-1.87222c-1.48667 0-1.87061.12794-2.23259.32153-.33585.17961-.59404.4378-.77365.77365-.19359.36198-.32153.74592-.32153 2.23259v7.54442c0 1.4867.12794 1.8706.32153 2.2326.17961.3359.4378.5941.77365.7737.36198.1936.74592.3215 2.23259.3215h7.54442c1.4867 0 1.8706-.1279 2.2326-.3215.3359-.1796.5941-.4378.7737-.7737.1936-.362.3215-.7459.3215-2.2326v-1.8722c0-.4971.4029-.9.9-.9s.9.4029.9.9v1.8722c0 1.5697-.1267 2.3193-.5343 3.0815-.3473.6495-.8625 1.1647-1.512 1.512-.7622.4076-1.5118.5343-3.0815.5343h-7.54442c-1.56965 0-2.31931-.1267-3.08147-.5343-.64953-.3473-1.16467-.8625-1.51204-1.512-.4076-.7622-.53426-1.5118-.53426-3.0815v-7.54442c0-1.56965.12666-2.31932.53426-3.08147.34738-.64953.86251-1.16466 1.51204-1.51204z"/><path d="m14 4.00001c0-.49706.403-.9.9-.9h5.1c.4971 0 .9.40294.9.9v5.1c0 .49705-.4029.89999-.9.89999-.497 0-.9-.40294-.9-.89999v-2.92721l-6.5636 6.5636c-.3514.3515-.9213.3515-1.2728 0-.3514-.3515-.3514-.9213 0-1.2728l6.5636-6.56359h-2.9272c-.497 0-.9-.40295-.9-.9z"/></g></svg>
+          <div class="caption">Сообщить об ошибке</div>
+        </a>
+      </div>
       <div class="author">made with love by <a href="https://t.me/tsivxrev" target="_blank" class="font-medium text-green-500">@tsivxrev</a></div>
     </div>
   </div>
--- src/tts.js
+++ src/tts.js
@@ -7,59 +7,70 @@ import customParseFormat from 'dayjs/plugin/customParseFormat';
 dayjs.locale('ru');
 dayjs.extend(customParseFormat);
 
-const tts = axios.create({
-  baseURL: 'https://api.tgt72.ru/api/v5',
-  transformResponse: [(data) => {
-    try {
-      // eslint-disable-next-line no-param-reassign
-      data = JSON.parse(data);
-    } catch {
-      return { error: true, message: 'Возможно, номер карты введен неправильно или же ведутся технические работы на сервере. Также не исключено, что карта заблокирована.', details: data };
-    }
-
-    if (data.card) {
-      const card = {
-        id: data.card,
-        type: data.ltype,
-        date: data.date,
-        balance: data.balance
-          .toFixed(2)
-          .toString()
-          .replace(/\B(?=(\d{3})+(?!\d))/g, ' '),
-
-        exemption: {
-          balance: data.exemption_balance,
-          limit: data.exemption_limit,
-        },
-
-        history: data.history.map((item) => {
-          const isTravel = !item.agent.match(/ТТС/);
-
-          return {
-            ...item,
-
-            routeNumber: item.route.split('.')[0],
-            routeName: item.route.replace(`${item.route.split('.')[0]}.`, '').trim(),
-            isTravel,
-            parsedSum: `${isTravel ? '-' : '+'}${item.sum.toFixed(2)} ₽`,
-            parsedDate: dayjs(item.date, 'DD.MM.YY hh:mm:ss')
-              .locale('ru')
-              .format('DD MMMM в HH:mm'),
-
-          };
-        }).reverse(),
-
-        raw: data,
-      };
-      return card;
-    }
-    return data;
-  }],
+const instance = axios.create({
+  baseURL: 'https://api.tgt72.ru/api/',
 });
 
-export const getCard = (id) => {
-  const hash = MD5(`${dayjs().format('DD.MM.YYYY')}.${id}`).toString();
-  return tts.get('/balance', { params: { card: id, hash } });
+class APIError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = 'APIError';
+    this.title = message.title;
+    this.message = message.message;
+    this.details = message.details;
+  }
+}
+
+export const todo = 'todo';
+export const vgetCard = async (id) => {
+  const date = dayjs().format('DD.MM.YYYY');
+  const hash = MD5(`${date}.${id}`).toString();
+
+  try {
+    const response = await instance.get('/v5/balance', {
+      params: {
+        card: id,
+        hash,
+      },
+    });
+
+    if (typeof response.data === 'string') {
+      throw new Error(response.data);
+    }
+
+    const card = {
+      id: response.data.card,
+      type: response.data.ltype,
+      updated_at: response.data.date,
+      balance: response.data.balance.toFixed(2), // .toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' '),
+      exemption: {
+        balance: response.data.exemption_balance,
+        limit: response.data.exemption_limit,
+      },
+      history_summary: 0,
+      history: response.data.history.map((event) => ({
+        agent: event.agent,
+        balance: event.balance,
+        date: dayjs(event.date, 'DD.MM.YY hh:mm:ss'),
+        sum: event.sum.toFixed(2),
+        type: event.type,
+        exemption_balance: event.exemption_balance,
+        route_number: event.route.split('.')[0],
+        route_name: event.route.replace(`${event.route.split('.')[0]}.`, '').trim(),
+      })).reverse(),
+    };
+
+    card.history.forEach((event) => { card.history_summary += +event.sum; });
+    card.history_summary = card.history_summary.toFixed(2);
+
+    return card;
+  } catch (error) {
+    throw new APIError({
+      title: 'Произошла ошибка на сервере',
+      message: 'Проверьте правильность написания номера карты и повторите попытку позже.',
+      details: error?.response?.data || error.message,
+    });
+  }
 };
 
-export const getNews = (limit = 15) => tts.get('/news', { params: { limit } });
+window.card = vgetCard;
